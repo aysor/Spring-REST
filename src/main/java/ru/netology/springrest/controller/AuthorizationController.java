b6cab68f9a9dfaec1875df5a58df714282881163
AyselS <diospirorf@yandex.ru>
2025-06-02 08:55:08 +0300

    nginx configuration

--- src/main/java/ru/netology/springrest/controller/AuthorizationController.java
+++ src/main/java/ru/netology/springrest/controller/AuthorizationController.java
@@ -2,12 +2,16 @@ package ru.netology.springrest.controller;
 
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.*;
 import ru.netology.springrest.model.Authorities;
 import ru.netology.springrest.model.User;
 import ru.netology.springrest.service.AuthorizationService;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class AuthorizationController {
@@ -25,5 +29,26 @@ public class AuthorizationController {
     @GetMapping("/authorize")
     public List<Authorities> getAuthorities(@Valid User user) {
         return service.getAuthorities(user);
+    }
+
+    @PostMapping("/handle-form")
+    public ResponseEntity<?> handleForm(@RequestParam("targetController") String targetController,
+                                        @RequestBody MultiValueMap<String, String> formData) {
+        if("authorize".equals(targetController)) {
+            String name = formData.get("name").get(0);
+            String pwd = formData.get("password").get(0);
+            List<Authorities> authorities = getAuthorities(new User(name, pwd));
+            return ResponseEntity.ok("authorize");
+        } else if ("register".equals(targetController)) {
+            String name = formData.get("name").get(0);
+            String pwd = formData.get("password").get(0);
+            List<Authorities> authorities = Arrays.stream(formData.get("authorities").get(0).split(" "))
+                                                        .map(Authorities::valueOf)
+                                                        .collect(Collectors.toList());
+            register(new User(name, pwd, authorities));
+            return ResponseEntity.ok("register");
+        } else {
+            return ResponseEntity.badRequest().body("Invalid Controller");
+        }
     }
 }
